f07df380b8f4646cec7ff5b38936da8e3b1b3b93
clockworks001 <nag03412@nifty.ne.jp>
2014-08-11 10:27:21 +0900

    tutorial by dot_instorl

---
+++ hello05.py
@@ -0,0 +1,18 @@
+# coding: UTF-8
+# 文字列
+# "hello" 'hello'  "'どちらでもok
+# 日本語 u"こんにちは！"  → uを付ける 3では必要なし
+# +	連結
+# *	繰り返し
+# エスケープ	\n \t \\
+
+print "hello " + "world"#連結
+print "無駄！" * 10	#繰り返し
+print 'hello\n wo\trld\\ it\'s a pen'
+
+# 複数行にわたる文字列の例　"""で囲む"""
+print """<html lang="ja">
+<body>
+</body>
+</html>"""
+
---
+++ hello06.py
@@ -0,0 +1,15 @@
+# coding: UTF-8
+# •¶š—ñ
+# len •¶š”
+# find@ŒŸõ
+# []@Ø‚èo‚µ
+s = "abcdefghi"	
+print len(s)		#9
+print s.find("c")	#2
+print s.find("x")	#-1
+print s[2]		#c
+print s[2:5]		#cde
+print s[:5]		#abcde
+print s[5:]		#fghi
+print s[2:-1]		#cdefgh
+
